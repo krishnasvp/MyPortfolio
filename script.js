adac1f62fd9f35df118185a0356b3fb9167a31ba
Krishna Sivapothanathan <krishnasivapothanathan@gmail.com>
2024-01-20 13:11:01 +0530

    typed plugin added to website - portfolio intro section

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ MenuBtn.addEventListener('click', function(e){
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark')
 })
+
+// Typing Effect
+
+let typed = new Typed('.auto-input', {
+    strings: ['Front-End Dev','Musician','Affiliate Associate'],
+    typeSpeed: 110,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+})
