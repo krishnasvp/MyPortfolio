e41b4e429591d8ec07dd2ba4aab889bb89d7e457
Krishna Sivapothanathan <krishnasivapothanathan@gmail.com>
2024-02-06 00:01:29 +0530

    page scrolling update

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ let typed = new Typed('.auto-input', {
     backDelay: 2000,
     loop: true,
 })
+
+// Active link State on scroll
+
+// Get All Links
+let navLinks = document.querySelectorAll('nav ul li a')
+// Get All Sections
+let sections = document.querySelector('section')
+console.log(sections)
+
+window.addEventListener('scroll', function (){
+    const scrollPos = window.scrollY = 20
+    sections.forEach(section => {
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if(section.getAttribute('id') === link.getAttribute('href').substring(1)){
+                    link.classList.add('active')
+                }
+            })
+        }
+    })
+})
